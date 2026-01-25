32a5a2a17042c67c728f5790621eb152ae301146
Gamaliel Jimenez <egjimenezg@gmail.com>
2026-01-25 11:12:27 -0600

    Create a new A record pointing the domain to Github Pages IP addresses

--- route_53/gh-pages-dns/__main__.py
+++ route_53/gh-pages-dns/__main__.py
@@ -1,10 +1,33 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# See: https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site#configuring-an-apex-domain
+github_pages_a = [
+  "185.199.108.153",
+  "185.199.109.153",
+  "185.199.110.153",
+  "185.199.111.153"
+]
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+config = pulumi.Config()
+
+domain = config.require("domain")
+
+gh_pages_target = config.require("githubPagesTarget")
+
+zone = aws.route53.get_zone(name=domain.rstrip(".") + ".", private_zone=False)
+zone_id = zone.zone_id
+
+aws.route53.Record(
+  "apex-a",
+  zone_id=zone_id,
+  name=domain,
+  type="A",
+  ttl=300,
+  records=github_pages_a
+)
+
+pulumi.export("zoneId", zone_id)
+pulumi.export("domain", domain)
