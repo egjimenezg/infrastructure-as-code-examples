05a7141f31ff04881cc5754f8545dd792cf7ce6c
Gamaliel Jimenez <egjimenezg@gmail.com>
2026-03-01 01:12:15 -0600

    feat: create hosted zone for custom domain

--- route_53/gh-pages-dns/__main__.py
+++ route_53/gh-pages-dns/__main__.py
@@ -11,13 +11,24 @@ github_pages_a = [
   "185.199.111.153"
 ]
 
+github_pages_aaaa = [
+  "2606:50c0:8000::153",
+  "2606:50c0:8001::153",
+  "2606:50c0:8002::153",
+  "2606:50c0:8003::153",
+]
+
 config = pulumi.Config()
 
 domain = config.require("domain")
 
 gh_pages_target = config.require("githubPagesTarget")
 
-zone = aws.route53.get_zone(name=domain.rstrip(".") + ".", private_zone=False)
+zone = aws.route53.Zone(
+  "primary-zone",
+  name=domain
+)
+
 zone_id = zone.zone_id
 
 aws.route53.Record(
@@ -29,5 +40,24 @@ aws.route53.Record(
   records=github_pages_a
 )
 
+aws.route53.Record(
+  "apex-aaaa",
+  zone_id=zone.zone_id,
+  name=domain,
+  type="AAAA",
+  ttl=300,
+  records=github_pages_aaaa
+)
+
+aws.route53.Record(
+  "www-cname",
+  zone_id=zone_id,
+  name=f"www.{domain}",
+  type="CNAME",
+  ttl=300,
+  records=[gh_pages_target]
+)
+
 pulumi.export("zoneId", zone_id)
 pulumi.export("domain", domain)
+pulumi.export("www", f"www.{domain}")
